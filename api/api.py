f451b786df130179383918fe37258f83ba72ccee
Krishna Sangeeth <kskrishnasangeeth@gmail.com>
2017-08-19 16:59:37 +0530

    integration with models

--- api/api.py
+++ api/api.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import os
+import sys
+
+from style_transfer.neural_style import StylizeImage
 
 app = Flask("vangogh")
+
+def stylize_image(content_image,style_image,model):
+    si = StylizeImage(style_image, content_image, model, scale=3, output="stylized", cuda = False)
+    si.style_image()
+
 
 
 @app.route("/ping", methods=['GET'])
@@ -20,6 +28,7 @@ def generateArt():
                     <form method=post enctype=multipart/form-data>
                         <input type="file" name="style_file" />
                         <input type="file" name="content_file" />
+                        <input type="text" name ="model" />
                         <input type="submit" value="Submit"/>
                     </form>
                 </html>
@@ -28,11 +37,13 @@ def generateArt():
     print request.files
     style_file = request.files['style_file']
     content_file = request.files['content_file']
+    model = request.text['model']
     style_filename = secure_filename(style_file.filename)
     content_filename = secure_filename(content_file.filename)
-    style_file.save(os.path.join("/tmp/vangog", style_filename))
-    content_file.save(os.path.join("/tmp/vangog", content_filename))
+    style_file.save(os.path.join("/tmp/vangog", "style_image"))
+    content_file.save(os.path.join("/tmp/vangog", "content_image"))
 
+    stylize_image("content_image","style_image",model)
     return "ok"
 
 
--- style_transfer/neural_style.py
+++ style_transfer/neural_style.py
@@ -15,24 +15,36 @@ import utils
 from transformer_net import TransformerNet
 from vgg import Vgg16
 
-def stylize(args):
-    content_image = utils.load_image(args.content_image, scale=args.content_scale)
-    content_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255))
-    ])
-    content_image = content_transform(content_image)
-    content_image = content_image.unsqueeze(0)
-    if args.cuda:
-        content_image = content_image.cuda()
-    content_image = Variable(content_image, volatile=True)
+class StylizeImage(object):
+    def __init__(self,style_image,content_image,model,scale,output,cuda=False):
+        self.style_image = style_image
+        self.content_image = content_image
+        self.model = model
+        self.content_scale = scale
+        self.cuda = cuda
+        self.output_image = output
 
-    style_model = TransformerNet()
-    style_model.load_state_dict(torch.load(args.model))
-    if args.cuda:
-        style_model.cuda()
-    output = style_model(content_image)
-    if args.cuda:
-        output = output.cpu()
-    output_data = output.data[0]
-    utils.save_image(args.output_image, output_data)
+    def stylize(self):
+        content_image = utils.load_image(self.content_image, scale=self.content_scale)
+        content_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255))
+        ])
+        content_image = content_transform(content_image)
+        content_image = content_image.unsqueeze(0)
+        if self.cuda:
+            content_image = content_image.cuda()
+        content_image = Variable(content_image, volatile=True)
+
+        style_model = TransformerNet()
+        style_model.load_state_dict(torch.load(self.model))
+
+        if self.cuda:
+            style_model.cuda()
+        output = style_model(content_image)
+
+        if self.cuda:
+            output = output.cpu()
+        output_data = output.data[0]
+        utils.save_image(self.output_image, output_data)
+
