6f28f1dd1f1cae9fc09b44c57bd8cb0a7a52426c
rbs392 <rbs392@yahoo.com>
2017-08-20 04:15:28 +0530

    heroku deployment intial commit

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+easy_install virtualenv
+
+virtualenv ./vangogh
+
+source ./vangogh/bin/activate
+
+pip install http://download.pytorch.org/whl/cu75/torch-0.2.0.post1-cp27-cp27m-manylinux1_x86_64.whl 
+pip install torchvision 
+
+pip install -r requirements.txt
+export PYTHONPATH=./
 python api/api.sh
